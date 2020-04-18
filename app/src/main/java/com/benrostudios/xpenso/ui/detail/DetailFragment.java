9f99d7c6b1d2855ac4fa5ed26a035ca8e3b5614d
Hemanth Krishna <hkpdev008@gmail.com>
2020-04-18 20:57:25 +0530

    DetailInfo Frag Added

--- app/src/main/java/com/benrostudios/xpenso/ui/detail/DetailFragment.java
+++ app/src/main/java/com/benrostudios/xpenso/ui/detail/DetailFragment.java
@@ -1,8 +1,11 @@
 package com.benrostudios.xpenso.ui.detail;
 
 import androidx.activity.OnBackPressedCallback;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.res.ColorStateList;
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,13 +15,25 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.benrostudios.xpenso.R;
 import com.benrostudios.xpenso.db.Expenses;
+
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 import static com.benrostudios.xpenso.adapters.HistoryAdapter.PASS_TAG;
 
@@ -27,6 +42,23 @@ public class DetailFragment extends Fragment {
     private DetailViewModel mViewModel;
     private Expenses expenses;
 
+    @BindView(R.id.expense_detail)
+    TextView expense;
+    @BindView(R.id.entity_detail)
+    TextView entity;
+    @BindView(R.id.desc_detail)
+    TextView desc;
+    @BindView(R.id.payment_mode_detail)
+    TextView payment_mode;
+    @BindView(R.id.time_detail)
+    TextView time;
+    @BindView(R.id.transaction_type_detail)
+    TextView transactionType;
+    @BindView(R.id.delete_transaction)
+    Button delete;
+    @BindView(R.id.image_detail)
+    ImageView image;
+
     public static DetailFragment newInstance() {
         return new DetailFragment();
     }
@@ -34,21 +66,52 @@ public class DetailFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.detail_fragment, container, false);
+        View view = inflater.inflate(R.layout.detail_fragment, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         expenses = (Expenses) getArguments().getSerializable(PASS_TAG);
-        Toast.makeText(getContext(),expenses.getPerson(),Toast.LENGTH_LONG).show();
+        populateUI();
+
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(DetailViewModel.class);
+        mViewModel = new ViewModelProvider(this).get(DetailViewModel.class);
         // TODO: Use the ViewModel
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewModel.delete(expenses);
+                Navigation.findNavController(v).navigateUp();
+            }
+        });
+    }
+
+    private void populateUI() {
+        entity.setText(expenses.getPerson());
+        expense.setText("" + expenses.getAmount());
+        desc.setText(expenses.getDesc().isEmpty() ? "No Description Found" : expenses.getDesc());
+        payment_mode.setText(expenses.getPaymentMode());
+        transactionType.setText(expenses.getType());
+        time.setText(dateFormatter(expenses.getTime()));
+        entity.setText(expenses.getPerson());
+        entity.setText(expenses.getPerson());
+        if (expenses.getType().equals(getResources().getString(R.string.expenditure))) {
+            image.setImageTintList(ColorStateList.valueOf(getResources().getColor(R.color.red_money)));
+        } else {
+            image.setImageTintList(ColorStateList.valueOf(getResources().getColor(R.color.green_money)));
+        }
+    }
+
+    private String dateFormatter(Date date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("h:mm aa dd MMM/yyyy");
+        return sdf.format(date);
     }
 
 }
--- app/src/main/java/com/benrostudios/xpenso/ui/detail/DetailViewModel.java
+++ app/src/main/java/com/benrostudios/xpenso/ui/detail/DetailViewModel.java
@@ -1,7 +1,34 @@
 package com.benrostudios.xpenso.ui.detail;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.ViewModel;
 
-public class DetailViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+import com.benrostudios.xpenso.db.Expenses;
+import com.benrostudios.xpenso.repo.ExpensesRepository;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Date;
+
+public class DetailViewModel extends AndroidViewModel {
+
+    private ExpensesRepository repo;
+    public DetailViewModel(@NonNull Application application) {
+        super(application);
+        repo = new ExpensesRepository(application);
+    }
+
+    public void delete(Expenses expenses){
+        repo.delete(expenses);
+        firebaseDelete(expenses.getTime());
+    }
+
+    private void firebaseDelete(Date time){
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference(FirebaseAuth.getInstance().getUid()+"/"+time.toString());
+        database.removeValue();
+    }
 }
